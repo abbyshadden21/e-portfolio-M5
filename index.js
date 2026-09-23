6bfe1382385475ab30943c6a9581f72ab97216bb
abbyshadden21 <lilianaaramirez9@gmail.com>
2026-09-22 22:50:28 -0500

    first push

---
+++ index.js
@@ -0,0 +1,60 @@
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;                  
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+  
+  for (let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = 'translate(${x * boolInt}px, ${y *}px)'
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += "dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
+
+
+
+async function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector('.modal__overlay--loading');
+  const success = document.querySelector('.modal__overlay--success');
+  loading.classList += " modal__overlay--visible";
+ emailjs
+   .sendForm(
+    'service_ybtp32j',
+    'template_m40htej',
+    event.target,
+    'w7sgstcsYDvB3iUhM'
+   ) 
+   .then(() => {
+    loading.classList.remove("modal__overlay--visible");
+    success.classList += " modal__overlay--visible";
+   })
+  .catch(() => {
+    loading.classList.remove("modal__overlay--visible");
+    alert (
+      "The email service temporarily unavailable. Please contact me directly on email@email.com "
+    );
+   })
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal--open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal--open";
+}
